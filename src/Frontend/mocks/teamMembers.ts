b0d259a9dd340fc262281e722c70dccd30f4bfdd
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-03-06 11:02:24 -0300

    teamMember rutas actualizadas

--- src/Frontend/mocks/teamMembers.ts
+++ src/Frontend/mocks/teamMembers.ts
@@ -1,25 +1,25 @@
 export const teamMembers = [
   {
     id: 1,
-    img: "../../../src/assets/nataly-castaneda.webp",
+    img: "/nataly-castaneda.webp",
     name: "Nataly Castañeda",
     occupation: "Fullstack Developer",
   },
   {
     id: 2,
-    img: "../../../src/assets/alexander-castillo.webp",
+    img: "/alexander-castillo.webp",
     name: "Alexander Castillo",
     occupation: "Fullstack Developer",
   },
   {
     id: 3,
-    img: "../../../src/assets/emmanuel-cruz.webp",
+    img: "/emmanuel-cruz.webp",
     name: "Emmanuel Cruz",
     occupation: "Frontend Developer",
   },
   {
     id: 4,
-    img: "../../../src/assets/antonella-heimen.webp",
+    img: "/antonella-heimen.webp",
     name: "Antonella Heimen",
     occupation: "QA Tester",
   },
