6e2652ef57931a86377a33e7dcee92d41d55799f
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-03-06 11:04:20 -0300

    rutas de servicios actualizadas

--- src/Frontend/mocks/services.ts
+++ src/Frontend/mocks/services.ts
@@ -1,21 +1,21 @@
 export const services = [
   {
     id: 1,
-    img: "../../../src/assets/services-img1.avif",
+    img: "/services-img1.avif",
     title: "Almacenamiento Seguro",
     paragraph:
       "Tus contraseñas se guardan con cifrado de nivel militar, protegiendo tu información contra accesos no autorizados.",
   },
   {
     id: 2,
-    img: "../../../src/assets/services-img2.avif",
+    img: "/services-img2.avif",
     title: "Alerta de Filtraciones",
     paragraph:
       "Recibe notificaciones si alguna de tus contraseñas ha sido comprometida y actualízala de inmediato.",
   },
   {
     id: 3,
-    img: "../../../src/assets/services-img3.avif",
+    img: "/services-img3.avif",
     title: "Autocompletado Rápido",
     paragraph:
       "Accede a tus cuentas de forma instantánea con nuestro autocompletado seguro y sin complicaciones.",
