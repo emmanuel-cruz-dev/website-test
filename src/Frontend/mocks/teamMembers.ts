c2b1bb869767b8ed081c4a1890c89c452780d97d
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-03-07 08:00:52 -0300

    teamMembers.ts modificado

--- src/Frontend/mocks/teamMembers.ts
+++ src/Frontend/mocks/teamMembers.ts
@@ -7,20 +7,8 @@ export const teamMembers = [
   },
   {
     id: 2,
-    img: "/alexander-castillo.webp",
-    name: "Alexander Castillo",
-    occupation: "Fullstack Developer",
-  },
-  {
-    id: 3,
     img: "/emmanuel-cruz.webp",
     name: "Emmanuel Cruz",
     occupation: "Frontend Developer",
-  },
-  {
-    id: 4,
-    img: "/antonella-heimen.webp",
-    name: "Antonella Heimen",
-    occupation: "QA Tester",
   },
 ];
