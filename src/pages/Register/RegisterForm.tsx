3e456bc96abef9166f01a9b76960b7266d3974b9
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-05-03 12:31:17 -0300

    feat: input con icono mostrar/ocultar contraseña agregado a RegisterForm

--- src/pages/Register/RegisterForm.tsx
+++ src/pages/Register/RegisterForm.tsx
@@ -1,6 +1,11 @@
+import { Eye, EyeOff } from "lucide-react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function RegisterForm() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
   return (
     <form className="space-y-4 text-black">
       <div>
@@ -13,26 +18,48 @@ function RegisterForm() {
         />
       </div>
       <div>
-        <div className="relative">
+        <div className="relative w-full max-w-md">
           <input
-            type="password"
+            id="password"
             name="password"
+            type={showPassword ? "text" : "password"}
+            className="mt-1 w-full pr-10 rounded-md border px-3 py-2"
             placeholder="Contraseña*"
-            className="mt-1 w-full rounded-md border px-3 py-2"
             required
           />
+          <button
+            type="button"
+            className="absolute top-4 right-3 text-gray-500 hover:text-gray-700"
+            onClick={() => setShowPassword(!showPassword)}
+            aria-label={
+              showPassword ? "Ocultar contraseña" : "Mostrar contraseña"
+            }
+          >
+            {showPassword ? <Eye size={20} /> : <EyeOff size={20} />}
+          </button>
         </div>
       </div>
 
       <div>
-        <div className="relative">
+        <div className="relative w-full max-w-md">
           <input
-            type="password"
+            id="password"
             name="password"
+            type={showConfirmPassword ? "text" : "password"}
+            className="mt-1 w-full pr-10 rounded-md border px-3 py-2"
             placeholder="Confirmar contraseña*"
-            className="mt-1 w-full rounded-md border px-3 py-2"
             required
           />
+          <button
+            type="button"
+            className="absolute top-4 right-3 text-gray-500 hover:text-gray-700"
+            onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+            aria-label={
+              showConfirmPassword ? "Ocultar contraseña" : "Mostrar contraseña"
+            }
+          >
+            {showConfirmPassword ? <Eye size={20} /> : <EyeOff size={20} />}
+          </button>
         </div>
       </div>
 
