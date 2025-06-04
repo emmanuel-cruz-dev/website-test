0df9b610dbe048c0bf0698d0c3eb4e21b1d72c8d
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-04 14:18:28 -0300

    feat: eliminates GenericTable from Patients component

--- src/pages/Patients/Patients.tsx
+++ src/pages/Patients/Patients.tsx
@@ -1,39 +1,8 @@
-import patients from "../../data/patients.json";
-import {
-  GenericTable,
-  Patient,
-} from "../OurServices/Professionals/Professionals";
-
 function Patients() {
   return (
     <section>
       <article className="container text-center">
         <h2 className="font-semibold text-3xl my-10">Pacientes</h2>
-        <GenericTable<Patient>
-          items={patients}
-          columns={[
-            { key: "name", header: "Nombre" },
-            { key: "lastName", header: "Apellido" },
-            { key: "dni", header: "DNI" },
-            {
-              key: "hasInsurance",
-              header: "Tiene seguro",
-              render: (item) => (item.hasInsurance ? "Sí" : "No"),
-            },
-            { key: "paymentType", header: "Método de pago" },
-            {
-              key: "photoUrl",
-              header: "Foto",
-              render: (item) => (
-                <img
-                  src={item.photoUrl}
-                  alt="Foto"
-                  className="w-12 h-12 rounded-full"
-                />
-              ),
-            },
-          ]}
-        />
       </article>
     </section>
   );
