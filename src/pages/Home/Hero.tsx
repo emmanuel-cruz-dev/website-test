d279887869c9bebff9567d05efa145a0da8424fc
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-19 14:45:32 -0300

    feat: updates styles and structure in Hero component

--- src/pages/Home/Hero.tsx
+++ src/pages/Home/Hero.tsx
@@ -1,5 +1,16 @@
+import CocaColaImg from "../../assets/coca-cola-png.png";
+
 function Hero() {
-  return <section className="h-screen">Hero</section>;
+  return (
+    <section className="py-6 px-2">
+      <article className="py-4 px-2 grid grid-cols-2 items-center bg-gradient-to-br from-sky-200 to-pink-200 rounded-2xl">
+        <figure className="w-32">
+          <img className="w-full object-cover" src={CocaColaImg} alt="" />
+        </figure>
+        <h1 className="text-4xl font-bold">Ofertas de la semana</h1>
+      </article>
+    </section>
+  );
 }
 
 export default Hero;
