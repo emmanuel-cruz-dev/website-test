ba9a5ad0552489dda3dc34e9b7868f0b211d082e
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-20 17:34:56 -0300

    feat: add Filters component to components folder

---
+++ src/components/Filters.tsx
@@ -0,0 +1,14 @@
+function Filters() {
+  return (
+    <div>
+      <select name="" id="">
+        <option value="">Marca</option>
+        <option value="">Promocion</option>
+        <option value="">Mayor precio</option>
+        <option value="">Menor precio</option>
+      </select>
+    </div>
+  );
+}
+
+export default Filters;
