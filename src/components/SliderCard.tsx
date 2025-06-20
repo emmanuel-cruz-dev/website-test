b501477691c49ba40e88f0883f2fb7e756d91478
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-20 14:29:20 -0300

    feat: adds interface props to SliderCard component and updates styles

--- src/components/SliderCard.tsx
+++ src/components/SliderCard.tsx
@@ -1,6 +1,20 @@
-function SliderCard() {
+interface SliderCardProps {
+  title: string;
+  image: string;
+}
+
+function SliderCard({ image, title }: SliderCardProps) {
   return (
-    <article className="bg-[#f6c06e] rounded-2xl h-40">SliderCard</article>
+    <a href="#" className="bg-[#f6c06e] rounded-2xl py-3">
+      <figure className="max-w-16 md:max-w-24 mx-auto mb-2 md:mb-4">
+        <img
+          className="w-full object-cover"
+          src={image}
+          alt={`Icono ${title}`}
+        />
+      </figure>
+      <h1 className="md:text-2xl font-bold text-center">{title}</h1>
+    </a>
   );
 }
 
