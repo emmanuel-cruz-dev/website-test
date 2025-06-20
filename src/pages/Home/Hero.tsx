b6df4319ec6c571fde510343d262423ccd5a8764
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-20 10:47:59 -0300

    style: updates width in Hero section

--- src/pages/Home/Hero.tsx
+++ src/pages/Home/Hero.tsx
@@ -2,10 +2,14 @@ import CocaColaImg from "../../assets/coca-cola-png.png";
 
 function Hero() {
   return (
-    <section className="py-6 px-2">
+    <section className="py-6 px-2 max-w-2xl mx-auto">
       <article className="py-4 px-2 grid grid-cols-2 items-center bg-gradient-to-br from-sky-200 to-pink-200 rounded-2xl">
         <figure className="w-32">
-          <img className="w-full object-cover" src={CocaColaImg} alt="" />
+          <img
+            className="w-full object-cover"
+            src={CocaColaImg}
+            alt="Botella de Coca Cola"
+          />
         </figure>
         <h1 className="text-4xl font-extrabold">Ofertas de la semana</h1>
       </article>
