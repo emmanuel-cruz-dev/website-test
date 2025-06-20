33ebb21ea197814f6621b81f2776bba412beb977
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-20 11:19:06 -0300

    feat: imports SliderProducts in App file

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "./layouts/Header/Header";
 import Footer from "./layouts/Footer/Footer";
-import Loading from "./components/Loading/Loading";
+import Loading from "./components/Loading";
 import Hero from "./pages/Home/Hero";
+import SliderProducts from "./components/SliderProducts";
 
 const Home = () => (
   <Suspense fallback={<Loading />}>
     <Hero />
+    <SliderProducts />
   </Suspense>
 );
 
