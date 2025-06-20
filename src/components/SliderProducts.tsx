61dd23a7301d34ae1bfbbeec8d5964e2ea21b4b7
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-20 14:28:34 -0300

    chore: modifies structure in SliderProducts

--- src/components/SliderProducts.tsx
+++ src/components/SliderProducts.tsx
@@ -1,12 +1,15 @@
 import SliderCard from "./SliderCard";
+import SnacksImg from "../assets/snacks.png";
+import LimpiezaImg from "../assets/limpieza.png";
+import LacteosImg from "../assets/lacteos.png";
 
 function SliderProducts() {
   return (
     <section className="lg:py-6 px-2 max-w-2xl mx-auto">
       <article className="grid grid-cols-3 gap-4 md:gap-8 lg:gap-16 items-center">
-        <SliderCard />
-        <SliderCard />
-        <SliderCard />
+        <SliderCard image={SnacksImg} title="Snacks" />
+        <SliderCard image={LacteosImg} title="Lácteos" />
+        <SliderCard image={LimpiezaImg} title="Limpieza" />
       </article>
     </section>
   );
