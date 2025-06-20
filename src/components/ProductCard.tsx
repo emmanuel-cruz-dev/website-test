1d02b6608b880a76274499fbc2850e3cb62c74ff
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-20 16:33:01 -0300

    feat: updates styles and structure in ProductCard component

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,19 +1,16 @@
+import { ProductCardProps } from "@/types/types";
 import { ShoppingCart } from "lucide-react";
 
-interface ProductCardProps {
-  img: string;
-}
-
-function ProductCard({ img }: ProductCardProps) {
+function ProductCard({ image, name, price }: ProductCardProps) {
   return (
-    <article>
-      <figure className="max-w-20">
-        <img src={img} alt="" />
+    <article className="cursor-grab">
+      <figure className="bg-[#f6c06e] rounded-2xl mb-2">
+        <img className="max-w-10/12 object-cover mx-auto" src={image} alt="" />
       </figure>
-      <footer>
-        <h2>Papas Lays</h2>
-        <p>$2.99</p>
-        <button className="flex gap-2 items-center">
+      <footer className="font-medium flex flex-col gap-2">
+        <h2 className="text-sm">{name}</h2>
+        <p>${price}</p>
+        <button className="flex text-sm gap-2 justify-center items-center bg-[#8de68a] p-3 px-6 rounded-3xl cursor-pointer">
           <ShoppingCart />
           Agregar
         </button>
