b09e905948acff110e293fce67db647bb385c227
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-25 12:52:09 -0300

    style: modifies padding inline in Header component

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,20 +1,23 @@
-import KramerIcon from "../../assets/kisco-kramer-icon.png";
 import { Link } from "react-router-dom";
+import { ShoppingCart } from "lucide-react";
+import LogoKramer from "../../assets/logo-kramer.png";
 
 function Header() {
   return (
-    <header className="header p-4 md:px-10 text-lg font-semibold shadow-2xl">
+    <header className="header p-4 py-2 md:px-10 text-lg font-semibold shadow-2xl">
       <div className="flex justify-between items-center max-w-2xl mx-auto">
         <Link to="/" className="flex items-center gap-2 z-10" title="Inicio">
           <img
-            className="max-w-10 object-cover"
-            src={KramerIcon}
+            className="max-w-18 object-cover"
+            src={LogoKramer}
             alt=""
-            width={200}
-            height={200}
+            width={600}
+            height={279}
           />
-          <p className="font-bold">Kramer</p>
         </Link>
+        <button className="cursor-pointer">
+          <ShoppingCart />
+        </button>
       </div>
     </header>
   );
