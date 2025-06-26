f6498ad6a66ae73f1bd23726005d3e516256af9a
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-26 13:47:47 -0300

    feat: adds ApiProductProps to types file

--- src/types/types.ts
+++ src/types/types.ts
@@ -9,3 +9,22 @@ export interface ProductCardProps {
   category: string;
   promotion: boolean;
 }
+
+interface ApiCategoryProps {
+  id: string;
+  name: string;
+  image: string;
+}
+
+export interface ApiProductsProps {
+  id: string;
+  name: string;
+  brand: string;
+  weight: string;
+  description: string;
+  price: string;
+  stock: number;
+  image: string | null;
+  status: string;
+  category: ApiCategoryProps;
+}
