457bc901c4454bcfc8e5ba7767bbf2889f969a9b
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-27 16:23:37 -0300

    feat: adds busqueda path and SearchPage to App component

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ const Home = lazy(() => import("./routes/Home"));
 const SnacksPage = lazy(() => import("./routes/SnacksPage"));
 const DrinksPage = lazy(() => import("./routes/DrinksPage"));
 const CleaningPage = lazy(() => import("./routes/CleaningPage"));
+const SearchPage = lazy(() => import("./routes/SearchPage"));
 const ShoppingCartPage = lazy(() => import("./routes/ShoppingCartPage"));
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
           <Route path="/snacks" element={<SnacksPage />} />
           <Route path="/bebidas" element={<DrinksPage />} />
           <Route path="/limpieza" element={<CleaningPage />} />
+          <Route path="/busqueda" element={<SearchPage />} />
           <Route path="/carrito" element={<ShoppingCartPage />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
