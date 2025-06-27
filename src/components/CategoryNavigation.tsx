f764068154b0c51c805ac5dd2e966fdc60668130
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-27 14:05:33 -0300

    refactor: renames SliderProducts to CategoryNavigation and SliderCard to NavigationCard in components folder

--- src/components/CategoryNavigation.tsx
+++ src/components/CategoryNavigation.tsx
@@ -1,18 +1,18 @@
-import SliderCard from "./SliderCard";
+import NavigationCard from "./NavigationCard";
 import SnacksImg from "../assets/snacks-icon.png";
 import LimpiezaImg from "../assets/limpieza-icon.png";
 import LacteosImg from "../assets/bebidas-icon.png";
 
-function SliderProducts() {
+function CategoryNavigation() {
   return (
     <section className="lg:py-6 px-4 max-w-2xl mx-auto pb-6">
       <article className="grid grid-cols-3 gap-10 md:gap-8 lg:gap-16 items-center">
-        <SliderCard image={SnacksImg} title="Snacks" />
-        <SliderCard image={LacteosImg} title="Bebidas" />
-        <SliderCard image={LimpiezaImg} title="Limpieza" />
+        <NavigationCard image={SnacksImg} title="Snacks" link="/snacks" />
+        <NavigationCard image={LacteosImg} title="Bebidas" link="/bebidas" />
+        <NavigationCard image={LimpiezaImg} title="Limpieza" link="/limpieza" />
       </article>
     </section>
   );
 }
 
-export default SliderProducts;
+export default CategoryNavigation;
--- src/components/NavigationCard.tsx
+++ src/components/NavigationCard.tsx
@@ -1,8 +1,9 @@
-import { SliderCardProps } from "../types/types";
+import { Link } from "react-router-dom";
+import { NavigationCardProps } from "../types/types";
 
-function SliderCard({ image, title }: SliderCardProps) {
+function NavigationCard({ image, title, link }: NavigationCardProps) {
   return (
-    <a href="#" className="bg-[#d2eafc] rounded-2xl py-3">
+    <Link to={link} className="bg-[#d2eafc] rounded-2xl py-3">
       <figure className="max-w-16 md:max-w-24 mx-auto my-2 md:mb-4">
         <img
           className="w-full object-cover drop-shadow-lg"
@@ -14,8 +15,8 @@ function SliderCard({ image, title }: SliderCardProps) {
         />
       </figure>
       <h1 className="md:text-2xl font-semibold text-center">{title}</h1>
-    </a>
+    </Link>
   );
 }
 
-export default SliderCard;
+export default NavigationCard;
