680578d7829d78b3f213ef617295f644ec2c4758
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-27 16:44:43 -0300

    feat: comments useLocation and classes in Footer component

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -2,22 +2,28 @@ import { House, Search, ShoppingCart } from "lucide-react";
 import { Link } from "react-router-dom";
 
 function Footer() {
+  // const location = useLocation();
+
   return (
     <footer className="footer p-4 pb-2 max-w-2xl mx-auto" id="footer">
       <article className="p-[5px] rounded-2xl bg-gradient-to-r from-[#dff3fd] via-[#f5f6f6] to-[#fdf1ea]">
         <ul className="flex justify-between items-center bg-white rounded-xl p-4 px-10">
           <li>
             <Link to="/" title="Inicio">
-              <House />
+              <House
+              // className={`w-10 h-10 p-1 rounded-lg ${
+              //   location.pathname === "/" ? "border-2 border-black" : ""
+              // }`}
+              />
             </Link>
           </li>
           <li>
-            <Link to="/busqueda">
+            <Link to="/busqueda" title="Buscar">
               <Search />
             </Link>
           </li>
           <li>
-            <Link to="/carrito">
+            <Link to="/carrito" title="Carrito">
               <ShoppingCart />
             </Link>
           </li>
