e18f61bc45804b8977cf5f0508978bd3e79dd7c2
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-27 13:58:45 -0300

    feat: adds SnacksPage and Navigate to Routes in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,8 @@
-import { Suspense } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Header from "./layouts/Header/Header";
 import Footer from "./layouts/Footer/Footer";
-import Loading from "./components/Loading";
-import Hero from "./pages/Home/Hero";
-import SliderProducts from "./components/SliderProducts";
-import Recommended from "./components/Recommended";
-import TwoForOneBanner from "./components/TwoForOneBanner";
-import SearchModal from "./components/SearchModal";
-
-const Home = () => (
-  <Suspense fallback={<Loading />}>
-    <Hero />
-    <SearchModal />
-    <SliderProducts />
-    <Recommended />
-    <TwoForOneBanner />
-  </Suspense>
-);
+import Home from "./routes/Home";
+import SnacksPage from "./routes/SnacksPage";
 
 function App() {
   return (
@@ -25,6 +10,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/snacks" element={<SnacksPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
       <Footer />
     </>
