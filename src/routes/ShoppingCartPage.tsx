d7566b6ad92c35002ba5cc04e63238a30533370e
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-27 16:04:34 -0300

    feat: adds and configures ShoppingCartPage component in routes folder

---
+++ src/routes/ShoppingCartPage.tsx
@@ -0,0 +1,12 @@
+import { Suspense } from "react";
+import Loading from "../components/Loading";
+import CategoryBanner from "../components/CategoryBanner";
+import CarritoBanner from "../assets/carrito-banner.png";
+
+const ShoppingCartPage = () => (
+  <Suspense fallback={<Loading />}>
+    <CategoryBanner title="Carrito" image={CarritoBanner} />
+  </Suspense>
+);
+
+export default ShoppingCartPage;
