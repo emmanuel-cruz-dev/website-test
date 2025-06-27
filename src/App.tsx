72d78442a2631f436c9dbaf23ca796b6f83f1233
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-27 15:23:39 -0300

    feat: adds Suspende and lazy loading to App component and updates routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import { lazy, Suspense } from "react";
 import Header from "./layouts/Header/Header";
 import Footer from "./layouts/Footer/Footer";
-import Home from "./routes/Home";
-import SnacksPage from "./routes/SnacksPage";
+import Loading from "./components/Loading";
+
+const Home = lazy(() => import("./routes/Home"));
+const SnacksPage = lazy(() => import("./routes/SnacksPage"));
+const DrinksPage = lazy(() => import("./routes/DrinksPage"));
+const CleaningPage = lazy(() => import("./routes/CleaningPage"));
 
 function App() {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/snacks" element={<SnacksPage />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/snacks" element={<SnacksPage />} />
+          <Route path="/bebidas" element={<DrinksPage />} />
+          <Route path="/limpieza" element={<CleaningPage />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </>
   );
