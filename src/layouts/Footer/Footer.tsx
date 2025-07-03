28e457982376de30bc3be3461375f274882e6655
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-03 16:28:10 -0300

    style: adds fixed class to footer component so that it is always visible

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -5,51 +5,57 @@ function Footer() {
   const location = useLocation();
 
   return (
-    <footer className="footer p-4 pb-2 max-w-2xl mx-auto" id="footer">
-      <article className="p-[5px] rounded-2xl bg-gradient-to-r from-[#dff3fd] via-[#f5f6f6] to-[#fdf1ea]">
-        <ul className="flex justify-between items-center bg-white rounded-xl pt-1 px-10">
-          <li>
-            <Link
-              to="/"
-              title="Inicio"
-              className={`inline-block p-2 rounded-lg transition-colors duration-300 border-2 ${
-                location.pathname === "/"
-                  ? "border-black"
-                  : "border-transparent hover:bg-gray-100"
-              }`}
-            >
-              <House size={22} />
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/busqueda"
-              title="Buscar"
-              className={`inline-block p-2 rounded-lg transition-colors duration-300 border-2 ${
-                location.pathname === "/busqueda"
-                  ? "border-black"
-                  : "border-transparent hover:bg-gray-100"
-              }`}
-            >
-              <Search size={22} />
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/carrito"
-              title="Carrito"
-              className={`inline-block p-3 rounded-lg transition-colors duration-300 border-2 ${
-                location.pathname === "/carrito"
-                  ? "border-black"
-                  : "border-transparent hover:bg-gray-100"
-              }`}
-            >
-              <ShoppingCart size={22} />
-            </Link>
-          </li>
-        </ul>
-      </article>
-    </footer>
+    <>
+      <footer
+        className="fixed bottom-0 z-10 footer p-4 pb-2 max-w-2xl mx-auto w-full"
+        id="footer"
+      >
+        <nav className="p-[5px] rounded-2xl bg-gradient-to-r from-[#dff3fd] via-[#f5f6f6] to-[#fdf1ea]">
+          <ul className="flex justify-between items-center bg-white rounded-xl pt-1 px-10">
+            <li>
+              <Link
+                to="/"
+                title="Inicio"
+                className={`inline-block p-2 rounded-lg transition-colors duration-300 border-2 ${
+                  location.pathname === "/"
+                    ? "border-black"
+                    : "border-transparent hover:bg-gray-100"
+                }`}
+              >
+                <House size={22} />
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/busqueda"
+                title="Buscar"
+                className={`inline-block p-2 rounded-lg transition-colors duration-300 border-2 ${
+                  location.pathname === "/busqueda"
+                    ? "border-black"
+                    : "border-transparent hover:bg-gray-100"
+                }`}
+              >
+                <Search size={22} />
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/carrito"
+                title="Carrito"
+                className={`inline-block p-3 rounded-lg transition-colors duration-300 border-2 ${
+                  location.pathname === "/carrito"
+                    ? "border-black"
+                    : "border-transparent hover:bg-gray-100"
+                }`}
+              >
+                <ShoppingCart size={22} />
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </footer>
+      <div className="h-16"></div>
+    </>
   );
 }
 
