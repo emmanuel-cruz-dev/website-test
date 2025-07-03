367441ae6dfa766a73bac7f49d3f3b7a37e864df
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-03 16:17:20 -0300

    feat: adds borders by location pathname in footer component

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -1,30 +1,50 @@
 import { House, Search, ShoppingCart } from "lucide-react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Footer() {
-  // const location = useLocation();
+  const location = useLocation();
 
   return (
     <footer className="footer p-4 pb-2 max-w-2xl mx-auto" id="footer">
       <article className="p-[5px] rounded-2xl bg-gradient-to-r from-[#dff3fd] via-[#f5f6f6] to-[#fdf1ea]">
-        <ul className="flex justify-between items-center bg-white rounded-xl p-4 px-10">
+        <ul className="flex justify-between items-center bg-white rounded-xl pt-1 px-10">
           <li>
-            <Link to="/" title="Inicio">
-              <House
-              // className={`w-10 h-10 p-1 rounded-lg ${
-              //   location.pathname === "/" ? "border-2 border-black" : ""
-              // }`}
-              />
+            <Link
+              to="/"
+              title="Inicio"
+              className={`inline-block p-2 rounded-lg transition-colors duration-300 border-2 ${
+                location.pathname === "/"
+                  ? "border-black"
+                  : "border-transparent hover:bg-gray-100"
+              }`}
+            >
+              <House size={22} />
             </Link>
           </li>
           <li>
-            <Link to="/busqueda" title="Buscar">
-              <Search />
+            <Link
+              to="/busqueda"
+              title="Buscar"
+              className={`inline-block p-2 rounded-lg transition-colors duration-300 border-2 ${
+                location.pathname === "/busqueda"
+                  ? "border-black"
+                  : "border-transparent hover:bg-gray-100"
+              }`}
+            >
+              <Search size={22} />
             </Link>
           </li>
           <li>
-            <Link to="/carrito" title="Carrito">
-              <ShoppingCart />
+            <Link
+              to="/carrito"
+              title="Carrito"
+              className={`inline-block p-3 rounded-lg transition-colors duration-300 border-2 ${
+                location.pathname === "/carrito"
+                  ? "border-black"
+                  : "border-transparent hover:bg-gray-100"
+              }`}
+            >
+              <ShoppingCart size={22} />
             </Link>
           </li>
         </ul>
