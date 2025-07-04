b40ccb6015b1aa08dd70605b228eec5e771302e7
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-04 10:16:05 -0300

    refactor: updates name CategoryNavigationSlider in Home component

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,7 @@
 import { Suspense } from "react";
 import Loading from "../components/Loading";
 import SearchModal from "../components/SearchModal";
-import CategoryNavigation from "../pages/Home/CategoryNavigation";
+import CategoryNavigationSlider from "../pages/Home/CategoryNavigationSlider";
 import Recommended from "../pages/Home/Recommended";
 import TwoForOneBanner from "../components/TwoForOneBanner";
 import HeroSlider from "../pages/Home/HeroSlider";
@@ -10,7 +10,7 @@ const Home = () => (
   <Suspense fallback={<Loading />}>
     <HeroSlider />
     <SearchModal />
-    <CategoryNavigation />
+    <CategoryNavigationSlider />
     <Recommended />
     <TwoForOneBanner />
   </Suspense>
