1e729d9d00016e7f529ab69a35d8fab391808d0a
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-04 11:05:16 -0300

    feat: adds new routes to App router component

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,11 @@ const Home = lazy(() => import("./routes/Home"));
 const SnacksPage = lazy(() => import("./routes/SnacksPage"));
 const DrinksPage = lazy(() => import("./routes/DrinksPage"));
 const CleaningPage = lazy(() => import("./routes/CleaningPage"));
+const GroceryStorePage = lazy(() => import("./routes/GroceryStorePage"));
+const BakeryPage = lazy(() => import("./routes/BakeryPage"));
+const DeliPage = lazy(() => import("./routes/DeliPage"));
+const BookStorePage = lazy(() => import("./routes/BookStorePage"));
+const GiftShopPage = lazy(() => import("./routes/GiftShopPage"));
 const SearchPage = lazy(() => import("./routes/SearchPage"));
 const ShoppingCartPage = lazy(() => import("./routes/ShoppingCartPage"));
 
@@ -21,6 +26,11 @@ function App() {
           <Route path="/snacks" element={<SnacksPage />} />
           <Route path="/bebidas" element={<DrinksPage />} />
           <Route path="/limpieza" element={<CleaningPage />} />
+          <Route path="/almacen" element={<GroceryStorePage />} />
+          <Route path="/panaderia" element={<BakeryPage />} />
+          <Route path="/fiambreria" element={<DeliPage />} />
+          <Route path="/libreria" element={<BookStorePage />} />
+          <Route path="/regaleria" element={<GiftShopPage />} />
           <Route path="/busqueda" element={<SearchPage />} />
           <Route path="/carrito" element={<ShoppingCartPage />} />
           <Route path="*" element={<Navigate to="/" />} />
